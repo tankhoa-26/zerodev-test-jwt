49ecb5f192d89c731f0a021731862beaeddeba25
Kelvin <ttkhoa.hcmus@gmail.com>
2023-07-17 15:55:12 +0700

    Create file App.js to handle JWT by using Jose libarary

---
+++ app.js
@@ -0,0 +1,107 @@
+const fs = require("fs");
+const jose = require("jose");
+const jwktopem = require("jwk-to-pem");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const cors = require("cors");
+var express = require("express");
+var router = express();
+router.use(express.json());
+router.use(cors());
+
+router.get("/rotate/ec", async (req, res) => {
+	const { publicKey, privateKey } = await jose.generateKeyPair("PS256");
+
+	const privateJwk = await jose.exportJWK(privateKey);
+	const publicJwk = await jose.exportJWK(publicKey);
+
+	fs.writeFileSync(
+		"public_keys_ec.json",
+		JSON.stringify(publicJwk, null, "  ")
+	);
+	fs.writeFileSync(
+		"private_keys_ec.json",
+		JSON.stringify(privateJwk, null, "  ")
+	);
+
+	console.log("privateJwk: ", privateJwk);
+	console.log("publicJwk: ", publicJwk);
+
+	res.send("EC Key Rotated!!");
+});
+
+router.get("/jwks/ec", async (req, res) => {
+	const publicJwk = fs.readFileSync("public_keys_ec.json");
+	console.log(publicJwk.toString());
+
+	res.send(publicJwk.toString());
+});
+
+router.get("/.well-known/keys.json", async (req, res) => {
+	const publicJwk = fs.readFileSync("public_keys_ec.json");
+	console.log(publicJwk.toString());
+
+	res.send(publicJwk.toString());
+});
+
+router.get("/token/ec", async (req, res) => {
+	/**
+	 * basically this is a demo to make a JWKS endpoint , but you have change your JWT token creator with JWKS endpoint
+	 */
+	const alg = "RS256";
+	const privateJwk = fs.readFileSync("private_keys_ec.json");
+	const ecPrivateKey = await jose.importJWK(
+		JSON.parse(privateJwk.toString()),
+		"PS256"
+	);
+
+	const jwt = await new jose.SignJWT({ "urn:example:claim": true })
+		.setProtectedHeader({ alg })
+		.setIssuedAt()
+		.setIssuer("urn:example:issuer")
+		.setAudience("urn:example:audience")
+		.setExpirationTime("2h")
+		.sign(ecPrivateKey);
+
+	res.send({ jwt });
+});
+
+router.post("/verify/ec", async (req, res) => {
+	const { jwt } = req.body;
+	const publicJwk = fs.readFileSync("public_keys_ec.json");
+
+	const ecPublicKey = await jose.importJWK(
+		JSON.parse(publicJwk.toString()),
+		"PS256"
+	);
+
+	const { payload, protectedHeader } = await jose.jwtVerify(
+		jwt,
+		ecPublicKey,
+		{
+			issuer: "urn:example:issuer",
+			audience: "urn:example:audience",
+		}
+	);
+	console.log("payload: ", payload);
+	console.log("protectedHeader: ", protectedHeader);
+
+	if (payload[iss] === "urn:example:issuer") {
+		res.send(true);
+	}
+
+	// try {
+	// 	const decoded = jwt.verify(token, publicKey);
+	// 	res.send(decoded);
+	// } catch (e) {
+	// 	res.send({ error: e });
+	// }
+});
+
+const port = process.env.PORT || 3001;
+
+router.get("*", (req, res) => res.end("Hello"));
+
+router.listen(port, () => {
+	console.log(`Server is running on ${port}`);
+});
--- server.js
+++ server.js
@@ -19,73 +19,74 @@ router.use(cors());
 
 // This endpoint will rotate your Keys
 router.get("/rotate/ec", async (req, res) => {
-  const keyStore = jose.JWK.createKeyStore();
-  keyStore
-    .generate("EC", "P-256", {
-      alg: "ES256",
-      use: "sig",
-      x: "drUef3Fq_k496CUnMHWcwLeL8X6-z03Tg20tBlIQ1v4",
-      y: "0BTWRnwr5Sgo7iw3gpe-UO_vldShnvPDnrG80UUA5VA",
-      d: "0g5vAEKzugrXaRbgKG0Tj2qJ5lMP4Bezds1_sTybkfk",
-    })
-    .then((result) => {
-      fs.writeFileSync(
-        "keys_ec.json",
-        JSON.stringify(keyStore.toJSON(true), null, "  ")
-      );
-      console.log("keyStore", keyStore);
-      console.log("result", result);
-    });
-  res.send("EC Key Rotated");
+	const keyStore = jose.JWK.createKeyStore();
+	keyStore
+		.generate("EC", "P-256", {
+			alg: "ES256",
+			use: "sig",
+			x: "drUef3Fq_k496CUnMHWcwLeL8X6-z03Tg20tBlIQ1v4",
+			y: "0BTWRnwr5Sgo7iw3gpe-UO_vldShnvPDnrG80UUA5VA",
+			d: "0g5vAEKzugrXaRbgKG0Tj2qJ5lMP4Bezds1_sTybkfk",
+		})
+		.then((result) => {
+			fs.writeFileSync(
+				"keys_ec.json",
+				JSON.stringify(keyStore.toJSON(true), null, "  ")
+			);
+			// console.log("keyStore", keyStore.toJSON());
+			// console.log("result", result);
+		});
+	res.send("EC Key Rotated");
 });
 
 router.get("/rotate/rsa", async (req, res) => {
-  const keyStore = jose.JWK.createKeyStore();
-  keyStore
-    .generate("RSA", 2048, { alg: "RS256", use: "sig" })
-    .then((result) => {
-      fs.writeFileSync(
-        "keys_rsa.json",
-        JSON.stringify(keyStore.toJSON(true), null, "  ")
-      );
-    });
-  res.send("RSA Key Rotated");
+	const keyStore = jose.JWK.createKeyStore();
+	keyStore
+		.generate("RSA", 2048, { alg: "RS256", use: "sig" })
+		.then((result) => {
+			fs.writeFileSync(
+				"keys_rsa.json",
+				JSON.stringify(keyStore.toJSON(true), null, "  ")
+			);
+		});
+	res.send("RSA Key Rotated");
 });
 
 // router.get("/.well-known/keys.json", async (req, res) => {
-router.get("/jwks/rsa", async (req, res) => {
-  const ks = fs.readFileSync("keys_ec.json");
-  const keyStore = await jose.JWK.asKeyStore(ks.toString());
-  res.send(keyStore.toJSON());
+router.get("/jwks/ec", async (req, res) => {
+	const ks = fs.readFileSync("keys_ec.json");
+	const keyStore = await jose.JWK.asKeyStore(ks.toString());
+	res.send(keyStore.toJSON());
 });
 
 router.get("/.well-known/keys.json", async (req, res) => {
-  const ks = fs.readFileSync("keys_rsa.json");
-  const keyStore = await jose.JWK.asKeyStore(ks.toString());
+	const ks = fs.readFileSync("keys_ec.json");
+	const keyStore = await jose.JWK.asKeyStore(ks.toString());
 
-  res.send(keyStore.toJSON());
+	res.send(keyStore.toJSON());
 });
 
 router.get("/token/ec", async (req, res) => {
-  /**
-   * basically this is a demo to make a JWKS endpoint , but you have change your JWT token creator with JWKS endpoint
-   */
-  const ks = fs.readFileSync("keys_ec.json"); //change this with your JWKS endpoint https://your-domain/jwks/rsa by using api call or you can use like default
-  const keyStore = await jose.JWK.asKeyStore(ks.toString());
-  const [key] = keyStore.all({ use: "sig" });
+	/**
+	 * basically this is a demo to make a JWKS endpoint , but you have change your JWT token creator with JWKS endpoint
+	 */
+	const ks = fs.readFileSync("keys_ec.json"); //change this with your JWKS endpoint https://your-domain/jwks/rsa by using api call or you can use like default
+	const keyStore = await jose.JWK.asKeyStore(ks.toString());
+	const [key] = keyStore.all({ use: "sig" });
 
-  const payload = JSON.stringify({
-    sub: "123",
-    aud: "urn:zerodev:client",
-  });
-  // add below logic with your login token
-  const token = await jose.JWS.createSign(
-    { alg: "ES256", format: "compact" },
-    key
-  )
-    .update(payload, "utf8")
-    .final();
-  res.send({ token });
+	const payload = JSON.stringify({
+		iss: "kelvin",
+		email: "ttkhoa@gmail.com",
+		aud: "test-client",
+	});
+	// add below logic with your login token
+	const token = await jose.JWS.createSign(
+		{ alg: "ES256", format: "compact" },
+		key
+	)
+		.update(payload, "utf8")
+		.final();
+	res.send({ token });
 });
 
 // router.get("/token/rsa/:userID", async (req, res) => {
@@ -104,61 +105,64 @@ router.get("/token/ec", async (req, res) => {
 // });
 
 router.get("/token/rsa/:userID", async (req, res) => {
-  const { userID } = req.params;
-  const ks = fs.readFileSync("keys_rsa.json");
-  const keyStore = await jose.JWK.asKeyStore(ks.toString());
-  const [key] = keyStore.all({ use: "sig" });
+	const { userID } = req.params;
+	const ks = fs.readFileSync("keys_rsa.json");
+	const keyStore = await jose.JWK.asKeyStore(ks.toString());
+	const [key] = keyStore.all({ use: "sig" });
 
-  const opt = { compact: true, jwk: key, fields: { typ: "jwt" } };
-  const payload = JSON.stringify({
-    sub: "123",
-    aud: "urn:zerodev:client",
-    userID,
-    timestamp: Date.now(), // Include a timestamp for uniqueness
-  });
+	const opt = { compact: true, jwk: key, fields: { typ: "jwt" } };
+	const payload = JSON.stringify({
+		sub: "123",
+		aud: "urn:zerodev:client",
+		userID,
+		timestamp: Date.now(), // Include a timestamp for uniqueness
+	});
 
-  try {
-    const token = await jose.JWS.createSign(opt, key).update(payload).final();
-    res.send({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error generating token");
-  }
-});
-
-router.post("/verify/rsa", async (req, res) => {
-  const { token } = req.body;
-  const { data } = await axios.get(
-    "https://jwks-production.up.railway.app/jwks/rsa"
-  );
-  const [firstKey] = data.keys;
-  const publicKey = jwktopem(firstKey);
-  try {
-    const decoded = jwt.verify(token, publicKey);
-    res.send(decoded);
-  } catch (e) {
-    res.send({ error: e });
-  }
+	try {
+		const token = await jose.JWS.createSign(opt, key)
+			.update(payload)
+			.final();
+		res.send({ token });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Error generating token");
+	}
 });
 
 router.post("/verify/ec", async (req, res) => {
-  const { token } = req.body;
-  const { data } = await axios.get(
-    "https://jwks-production.up.railway.app/.well-known/keys.json"
-  );
-  const [firstKey] = data.keys;
-  const publicKey = jwktopem(firstKey);
-  try {
-    const decoded = jwt.verify(token, publicKey);
-    res.send(decoded);
-  } catch (e) {
-    res.send({ error: e });
-  }
+	const { token } = req.body;
+	const { data } = await axios.get(
+		// "https://jwks-production.up.railway.app/jwks/rsa"
+		"127.0.0.1:3001/.well-known/keys.json"
+	);
+	const [firstKey] = data.keys;
+	const publicKey = jwktopem(firstKey);
+	try {
+		const decoded = jwt.verify(token, publicKey);
+		res.send(decoded);
+	} catch (e) {
+		res.send({ error: e });
+	}
 });
-const port = process.env.PORT || 3000;
+
+router.post("/verify/ec", async (req, res) => {
+	const { token } = req.body;
+	const { data } = await axios.get(
+		"https://jwks-production.up.railway.app/.well-known/keys.json"
+	);
+	const [firstKey] = data.keys;
+	const publicKey = jwktopem(firstKey);
+	try {
+		const decoded = jwt.verify(token, publicKey);
+		res.send(decoded);
+	} catch (e) {
+		res.send({ error: e });
+	}
+});
+const port = process.env.PORT || 3001;
 
 router.get("*", (req, res) => res.end("Hello"));
 
 router.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+	console.log(`Server is running on ${port}`);
 });
